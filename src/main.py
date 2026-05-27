54af9dbbb05aacf4f81d74bdf6a0e448bb7e68c1
xXEDUARDO9Xx <166721569+xXEDUARDO9Xx@users.noreply.github.com>
2026-05-27 13:28:32 -0600

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,6 @@
+import functions_framework
+
+@functions_framework.cloud_event
+def gcs_metadata_extractor(cloud_event):
+    # TODO: Implementar la extracción de metadatos y Cloud Logging
+    pass
